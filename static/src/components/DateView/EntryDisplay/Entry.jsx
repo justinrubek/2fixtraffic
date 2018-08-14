fcc0b55d43c6c3c62f94ce82a0c709aa2bc6a751
Justin Rubek <rubejus@bvu.edu>
2018-08-14 13:40:01 -0500

    Arbitrary data from db now displayed when an Entry is clicked

--- static/src/components/DateView/EntryDisplay/Entry.jsx
+++ static/src/components/DateView/EntryDisplay/Entry.jsx
@@ -5,9 +5,34 @@ import axios from "axios";
 import style from "./style.css";
 
 export default class Entry extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+
+    this.state = {
+      expanded: false
+    };
+  }
+
+  onClick(new_expanded_val) {
+    console.log("entry clicked");
+    this.setState({ expanded: new_expanded_val });
+    if (this.props.onClick != null) {
+      this.props.onClick(!expanded);
+    }
+  }
+
   render() {
-    return (
-      <li key={this.props.id} className={style.cell}>
+    let expanded = false;
+    if (this.props.expanded != null) {
+      expanded = this.props.expanded;
+    } else {
+      expanded = this.state.expanded;
+    }
+
+    const basic_display = (
+      <div key={this.props.id} onClick={() => this.onClick(!expanded)}>
         {moment(this.props.time).format("hh:mm A")}
         <button
           className={`${style.remove_button} ${style.no_select}`}
@@ -18,7 +43,41 @@ export default class Entry extends React.Component {
         >
           x
         </button>
-      </li>
+      </div>
+    );
+
+    // Perhaps we need to change to always rendering this, and controlling the styling?
+    let expanded_display = null;
+    if (expanded) {
+      if (this.props.data != null) {
+        let data = [];
+        for (let name of Object.keys(this.props.data)) {
+          data.push(
+            <li key={name} className={style.data_group}>
+              <div key="data_name" className={style.data_title}>
+                {name}
+              </div>
+              <div key="data_value" className={style.data_content}>
+                {this.props.data[name]}
+              </div>
+            </li>
+          );
+        }
+
+        expanded_display = (
+          <div className={style.expansion}>
+            <ul className={style.list_no_bullet}>{data}</ul>
+          </div>
+        );
+      } else {
+        expanded_display = <p className={style.expansion}>No other data</p>;
+      }
+    }
+    return (
+      <div className={style.cell}>
+        {basic_display}
+        {expanded_display}
+      </div>
     );
   }
 }
